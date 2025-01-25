1827fc7d0da1feeb96f92259eda79a1cf7a8921b
Zahra Jalalkhan <zahrajid@gmail.com>
2025-01-25 17:06:57 +0300

    Update UploadMedia.js

--- src/pages/UploadMedia.js
+++ src/pages/UploadMedia.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './UploadMedia.css';
 
 const UploadMedia = () => {
   const [media, setMedia] = useState(null);
