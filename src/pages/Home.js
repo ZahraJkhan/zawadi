da51b40ecdba276ea68f34aa114eefd2d06dfdc5
Zahra Jalalkhan <zahrajid@gmail.com>
2025-01-25 17:00:41 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
     <div className="home">
-      <h2>Welcome to Zawadi!</h2>
-      <p>
-        In English, Zawadi translates to “Gift”. This interactive platform is a Gift to Mothers who can be able to digitally record and save their child’s developmental memories during their 1st year since birth.
-      </p>
+      <h1>Welcome to Zawadi</h1>
+      <p>Your digital record book for your baby’s milestones!</p>
+      <button>Login</button>
     </div>
   );
 };
